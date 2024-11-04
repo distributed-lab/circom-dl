f60b2eda67e122e3509a214a4304854e70807ba8
erdoganishe <khotyanmisha@gmail.com>
2024-11-04 15:39:53 +0200

    Add log2 and log2 Strict

--- circuits/int/arithmetic.circom
+++ circuits/int/arithmetic.circom
@@ -1,6 +1,7 @@
 pragma circom 2.1.6;
 
 include "../../node_modules/circomlib/circuits/comparators.circom";
+include "../../node_modules/circomlib/circuits/bitify.circom";
 
 template Inverse(){
     signal input in;
@@ -12,50 +13,96 @@ template Inverse(){
 //use if u don`t know what is len of bit representation of in[0] is
 template DivisionStrict(){
     signal input in[2];
-
-    signal output mod; //in[0] % in[1]
-    signal output div; //in[0] \ in[1]
-
+    
+    signal output mod;
+    signal output div;
+    
     mod <-- in[0] % in[1];
     div <-- in[0] \ in[1];
-
+    
     div * in[1] + mod === in[0];
     component check1 = LessEqThan(252);
     component check2 = GreaterThan(252);
-
+    
     check1.in[0] <== div * in[1];
     check1.in[1] <== in[0];
     check1.out === 1;
-
+    
     check2.in[0] <== (div + 1) * in[1];
     check2.in[1] <== in[0];
     check2.out === 1;
-
+    
 }
-
 
 //use this if u know what len of bit representation of in[1] is
 template Division(LEN){
-
+    
     assert (LEN < 253);
     signal input in[2];
-
-    signal output div; //in[0] \ in[1]
-    signal output mod; //in[0] % in[1]
-
+    
+    signal output div;
+    signal output mod;
+    
     mod <-- in[0] % in[1];
     div <-- in[0] \ in[1];
-
+    
     div * in[1] + mod === in[0];
     component check1 = LessEqThan(LEN);
     component check2 = GreaterThan(LEN);
-
+    
     check1.in[0] <== div * in[1];
     check1.in[1] <== in[0];
     check1.out === 1;
-
+    
     check2.in[0] <== (div + 1) * in[1];
     check2.in[1] <== in[0];
     check2.out === 1;
+    
+}
 
+//don`t use it for 0!!!
+template Log2CeilStrict(){
+    signal input in;
+    signal output out;
+    
+    signal bits[252];
+    component n2b = Num2Bits(252);
+    n2b.in <== in - 1;
+    n2b.out ==> bits;
+    
+    signal counter[252];
+    signal sum[252];
+    
+    counter[0] <== bits[251];
+    sum[0] <== counter[0];
+    
+    for (var i = 1; i < 252; i++){
+        counter[i] <== (1 - counter[i - 1]) * bits[251 - i] + counter[i - 1];
+        sum[i] <== sum[i - 1] + counter[i];
+    }
+    
+    out <== sum[251];
+}
+
+template Log2Ceil(RANGE){
+    signal input in;
+    signal output out;
+    
+    signal bits[RANGE];
+    component n2b = Num2Bits(RANGE);
+    n2b.in <== in - 1;
+    n2b.out ==> bits;
+    
+    signal counter[RANGE];
+    signal sum[RANGE];
+    
+    counter[0] <== bits[RANGE - 1];
+    sum[0] <== counter[0];
+    
+    for (var i = 1; i < RANGE; i++){
+        counter[i] <== (1 - counter[i - 1]) * bits[RANGE - 1 - i] + counter[i - 1];
+        sum[i] <== sum[i - 1] + counter[i];
+    }
+    
+    out <== sum[RANGE - 1];
 }
--- test/int.js
+++ test/int.js
@@ -1,4 +1,4 @@
-const { assert } = require("console");
+const { assert, log } = require("console");
 const path = require("path");
 
 const Scalar = require("ffjavascript").Scalar;
@@ -64,6 +64,42 @@ async function testDivisionStrict(input1, input2, circuit){
     }
 }
 
+async function testLog(input, circuit){
+    let real_result = [0n];
+
+    if (input != 1n){
+        real_result = BigInt(Math.log2(Number(input-1n))- Math.log2(Number(input-1n)) % 1 + 1);
+        real_result = [real_result]
+    }
+
+    const w = await circuit.calculateWitness({in: input}, true);
+
+    let circuit_result = w.slice(1, 1+1);
+
+    for (var i = 0; i < 1; i++){
+        assert(circuit_result[i] == real_result[i], `log(${input}), ${real_result[i]}, ${circuit_result[i]}`);
+    }
+
+}
+async function testLogRange(input, circuit){
+
+    let real_result = [0n];
+
+    if (input != 1n){
+        real_result = BigInt(Math.log2(Number(input-1n))- Math.log2(Number(input-1n)) % 1 + 1);
+        real_result = [real_result]
+    }
+
+    const w = await circuit.calculateWitness({in: input}, true);
+
+    let circuit_result = w.slice(1, 1+1);
+
+    for (var i = 0; i < 1; i++){
+        assert(circuit_result[i] == real_result[i], `log(${input}), ${real_result[i]}`);
+    }
+
+}
+
 describe("Inverse test", function () {
 
     this.timeout(100000);
@@ -126,4 +162,65 @@ describe("Division (strict) test", function () {
     it("140 / 20", async function () {
         await testDivisionStrict(140n, 20n, circuit);
     });
+});
+
+describe("Log2 (Strict) test", function () {
+
+    this.timeout(100000);
+    let circuit;
+
+    before(async () => {
+        circuit = await wasm_tester(path.join(__dirname, "circuits", "int", "log2Ceil.circom"));
+    });
+
+    it("log(1)", async function () {
+        await testLog(1n, circuit);
+    });
+
+    it("log(2)", async function () {
+        await testLog(2n, circuit);
+    });
+    it("log(127364283)", async function () {
+        await testLog(127364283n, circuit);
+    });
+
+    it("log(5)", async function () {
+        await testLog(5n, circuit);
+    });
+
+    it("log(4)", async function () {
+        await testLog(4n, circuit);
+    });
+
+});
+
+
+describe("Log2 test", function () {
+
+    this.timeout(100000);
+    let circuit;
+
+    before(async () => {
+        circuit = await wasm_tester(path.join(__dirname, "circuits", "int", "log2CeilRange.circom"));
+    });
+
+    it("log(1)", async function () {
+        await testLogRange(1n, circuit);
+    });
+
+    it("log(2)", async function () {
+        await testLogRange(2n, circuit);
+    });
+    it("log(127364283)", async function () {
+        await testLogRange(127364283n, circuit);
+    });
+
+    it("log(5)", async function () {
+        await testLogRange(5n, circuit);
+    });
+
+    it("log(4)", async function () {
+        await testLogRange(4n, circuit);
+    });
+
 });
