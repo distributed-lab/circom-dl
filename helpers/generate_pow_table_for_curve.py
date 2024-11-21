fc4505933fa3b9100dbbbc1b1f4506d75b648e31
Denys Riabtsev <68712122+1KitCat1@users.noreply.github.com>
2024-11-22 01:10:21 +0200

    EC operation & long arithmetics  (#2)
    * Project Inited
    
    * Add bigInt adding
    
    * Add optimised version of no carry mult for same lenght bignums
    
    * Add big int multiplying (same chunk number)
    
    * WIP: BigInt Comparators, add IsEqual
    
    * Add big num comparators
    
    * WIP: Modulus multiplication Add BigMultModP
    
    * Minor fix
    
    * Naming issue fixed
    
    * Add add and mult operations for big nums with different chunk sizes, fix comparators <, >=
    
    * Add mod and mult pow for bigint unequal chunks and tests for cases
    
    * Big Sub added
    
    * Add Big Sub + tests
    
    * Add Big sub for unequal chunks
    
    * Add all bit gates for 1 and 2 bits + tests
    
    * Add int arithmetic
    
    * Add log2 and log2 Strict
    
    * Add default comparators + tests
    
    * Add bitify
    
    * Secure + optimise equal chunks big int, optimised bitify
    
    * Update big int non equal chunks
    
    * Add bigInv and tests
    
    * WIP: Overflow bigInt
    
    * WIP: impoved some functions
    
    * Add curves, updated bigint, add comments to all
    
    * Remove or fix deprecated methods
    
    * Add tests for curve operations
    
    * Add elliptic curve addition
    
    * Change equal check in mods to smartEqual
    
    * Secure add and double for ec
    
    * Add optimised mod for oveflowed input
    
    * Remove unsecured metods
    
    * WIP: Generator multiplication: fail if last 8 bits are zeros
    
    * Add scalar mult without table
    
    * WIP: fix gen mult
    
    * Add scalar mult, gen mult, tests
    
    * Add tests for precompute scalar mult case
    
    * Added BrainpoolP256r1 support + test for those cases
    
    * Update curve.circom
    
    * Add ECDSA verification + tests
    
    ---------
    
    Co-authored-by: erdoganishe <khotyanmisha@gmail.com>

---
+++ helpers/generate_pow_table_for_curve.py
@@ -0,0 +1,145 @@
+import math
+import sys
+
+Gx = 0x8bd2aeb9cb7e57cb2c4b482ffc81b7afb9de27e1e3bd23c23a4453bd9ace3262
+Gy = 0x547ef835c3dac4fd97f8461a14611dc9c27745132ded8e545c1d54c72f046997
+P = 0xa9fb57dba1eea9bc3e660a909d838d726e3bf623d52620282013481d1f6e5377
+A = 0x7d5a0975fc2c3057eef67530417affe7fb8055c126dc5c6ce94a4b44f330b5d9
+B = 0x26dc5c6ce94a4b44f330b5d9bbd77cbf958416295cf7e1ce6bccdc18ff8c07b6
+
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+
+def double(x, y):
+    lamb = ((3 * (x ** 2) + A) * modinv(2 * y, P)) % P
+    retx = (lamb ** 2 - 2 * x) % P
+    rety = (lamb * (x - retx) - y) % P
+    return retx, rety
+
+
+def add(x1, y1, x2, y2):
+    lamb = ((y2 - y1) * modinv(P + x2 - x1, P)) % P
+    retx = (P + lamb ** 2 - x1 - x2) % P
+    rety = (P + lamb * (x1 - retx) - y1) % P
+    return retx, rety
+
+
+def get_g_pows(exp):
+    g_pows = []
+    curr_x, curr_y = Gx, Gy
+    for idx in range(exp):
+        g_pows.append((curr_x, curr_y))
+        curr_x, curr_y = double(curr_x, curr_y)
+    return g_pows
+
+
+def get_long(n, k, x):
+    ret = []
+    for idx in range(k):
+        ret.append(x % (2 ** n))
+        x = x // (2 ** n)
+    return ret
+
+
+def get_long_g_pows(exp, n, k):
+    g_pows = get_g_pows(exp)
+    long_g_pows = []
+    for x, y in g_pows:
+        long_x, long_y = get_long(n, k, x), get_long(n, k, y)
+        long_g_pows.append((long_x, long_y))
+    return long_g_pows
+
+
+def get_binary(x):
+    ret = []
+    while x > 0:
+        ret.append(x % 2)
+        x = x // 2
+    return ret
+
+def get_g_pow_val(g_pows, exp):
+    binary = get_binary(exp)
+    is_nonzero = False
+    curr_sum = None
+    for idx, val in enumerate(binary):
+        if val != 0:
+            if not is_nonzero:
+                is_nonzero = True
+                curr_sum = g_pows[idx]
+            else:
+                curr_sum = add(curr_sum[0], curr_sum[1],
+                               g_pows[idx][0], g_pows[idx][1])
+    return curr_sum
+
+
+def get_cache_str(n, k, stride):
+    num_strides = math.ceil(n * k / stride)
+    stride_cache_size = 2 ** stride
+    ret_str = '''
+function get_g_pow_stride{}_table(n, k) '''.format(stride)
+    ret_str = ret_str + '{'
+    ret_str = ret_str + '''
+    assert(n == {} && k == {});
+    var powers[{}][{}][2][{}];
+'''.format(n, k, num_strides, 2 ** stride, k)
+    EXP = 512 + stride
+    g_pows = get_g_pows(EXP)
+
+    for stride_idx in range(num_strides):
+        for idx in range(2 ** stride):
+            exp = idx * (2 ** (stride_idx * stride))
+            ret_append = '\n'
+            if exp > 0:
+                g_pow = get_g_pow_val(g_pows, exp)
+                long_g_pow = get_long(n, k, g_pow[0]), get_long(n, k, g_pow[1])
+                for reg_idx in range(k):
+                    ret_append += '    powers[{}][{}][0][{}] = {};\n'.format(
+                        stride_idx, idx, reg_idx, long_g_pow[0][reg_idx])
+                for reg_idx in range(k):
+                    ret_append += '    powers[{}][{}][1][{}] = {};\n'.format(
+                        stride_idx, idx, reg_idx, long_g_pow[1][reg_idx])
+            elif exp == 0:
+                for reg_idx in range(k):
+                    ret_append += '    powers[{}][{}][0][{}] = 0;\n'.format(
+                        stride_idx, idx, reg_idx)
+                for reg_idx in range(k):
+                    ret_append += '    powers[{}][{}][1][{}] = 0;\n'.format(
+                        stride_idx, idx, reg_idx)
+            ret_str = ret_str + ret_append
+    ret_str = ret_str + '''
+    return powers;
+}
+'''
+    return ret_str
+
+
+def get_ecdsa_func_str(n, k, stride_list):
+    ret_str = '''pragma circom 2.1.6;
+'''
+    for stride in stride_list:
+        cache_str = get_cache_str(n, k, stride)
+        ret_str = ret_str + cache_str
+    return ret_str
+
+
+def write_to_file():
+    stride_list = [8]
+    ecdsa_func_str = get_ecdsa_func_str(64, 4, stride_list)
+    with open('./tmp.circom', 'w') as file:
+        file.write(ecdsa_func_str)
+
+write_to_file()
